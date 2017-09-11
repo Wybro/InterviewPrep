d77df544551a67b0fc030f6171b0b6ecfb5c22fd
Connor <connor@wybro.com>
2017-09-11 17:43:49 -0400

    Add questions

---
+++ Questions/questions1.playground/Contents.swift
@@ -0,0 +1,66 @@
+/* 
+ 9/11/17
+ Interview Questions 2
+ 
+ Topics covered:
+ - Array / String processing
+ - Data Structures
+ - Run-time
+ - High-order functions
+*/
+
+
+
+/*
+ Question 1
+ - Find the MOST frequent integer(s) in an array
+ 
+ - Example:
+    let arr = [0,2,1,4,0,3] --> returns [0]
+*/
+
+
+
+/* -----------------------------------------------------------------------
+ Question 2
+ - Find the LEAST frequent integer(s) in an array
+ 
+ - Example:
+    let arr = [0,2,1,4,0,3] --> returns [2,1,4,3]
+*/
+
+
+
+/* -----------------------------------------------------------------------
+ Question 3
+ - Check whether or not a String contains all unique characters
+ 
+ - Example:
+    let str1 = "hi" --> returns true
+    let str2 = "hello" --> returns false
+*/
+
+
+
+/* -----------------------------------------------------------------------
+ Question 4
+ - Check if a String contains all the letters of another String
+ - Note: Don't worry about repeated letters -- count doesn't matter
+ - Note: Assume second String is the substring --> comparing that to first String
+ 
+ - Example:
+    let firstStr = "banana" , let secondStr = "band" --> returns false
+    let firstStr = "berries", let secondStr = "bees" --> returns true
+ 
+ *Bonus: Do this efficiently --> O(n) time after some pre-processing (assumes Strings are roughly same size)
+*/
+
+
+
+/* -----------------------------------------------------------------------
+ Question 5 -- High Order Functions (Hard)
+ - Given an array of potentially optional values (some nil), return an array with no nils
+ 
+ - Example:
+    let arr = [0,3,nil,2,nil] --> returns [0,3,2]
+*/
