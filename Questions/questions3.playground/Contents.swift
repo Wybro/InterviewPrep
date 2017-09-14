ef95ba0dfca10bd5868ad2dc9c8e2280fcc91136
Connor <connor@wybro.com>
2017-09-14 13:24:53 -0400

    Add substring question

---
+++ Questions/questions3.playground/Contents.swift
@@ -0,0 +1,20 @@
+/*
+ 9/14/17
+ Interview Questions 3
+ 
+ Topics covered:
+ - String manipulation (substrings)
+ ------------------------------------------------------------------------
+ */
+
+import Foundation
+
+/*
+ - Given: Array of strings of various lengths
+ - Task: Return array of strings where the last 2 characters are uppercased
+ * Hint: Need at least 2 characters to perform this
+ * Hint: Write function to make this change to a single string
+ * Hint: String indexing requires .index, where you have to specify str.startIndex / .endIndex
+    - There is a variant of this function that takes an offset
+*/
+
