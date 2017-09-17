9550bcd365fbd2e06c03a1dcb94773668bfaf513
Kyle <kyle@wybro.com>
2017-09-17 18:27:37 -0400

    Add more questions

---
+++ Questions/questions4.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+/* reverse a number
+
+ 123 -> 321
+ 
+ */
+
+
+
+
+/*
+ -------------------
+ Array of strings; want only the strings that are greater than 3 characters long
+ Of those, return a dictionary where the key represents the character and the value
+ represents the count of the character across the array
+ 
+ ex: let arr = ["a", "aa", "aaa", "aaaa"] -> ["a": 4]
+ */
+
+
+
+/*
+ -------------------
+ convert an array of ints into an array of strings
+ */
+
+
---
+++ Questions/questions6.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*
+ Given an array let arr = ["a","b","c","d","apple",nil,"cat","boy",nil]
+ Return a String that consists of each single-character doubled, all concatenated like "aabbccdd"
+ */
+
+
+
+
+/*
+ -------------------
+ Make a custom structure and have a collection of these structures. Then reduce the collection to a single element
+*/
+
+
+
+
+/*
+ -------------------
+ Given an array of strings, let strArr = ["apple","joy","work","space"], reduce each string down to it's substring of the last two characters of the string
+*/
+
