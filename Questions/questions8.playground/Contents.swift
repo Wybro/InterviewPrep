4da97b7d24464f087654eacdc7c4ca35dafc4ed6
Connor <connor@wybro.com>
2017-09-20 11:41:54 -0400

    Add question

---
+++ Questions/questions8.playground/Contents.swift
@@ -0,0 +1,9 @@
+/*
+ - Task:
+     - Create custom Car struct with properties: make (string), year (int)
+     - Sort cars by year
+ - Hints:
+     - Equatable & Comparable protocols are needed to define the comparison of custom struct
+ - Bonus points for custom print-out of Car struct (CustomStringConvertible)
+*/
+
