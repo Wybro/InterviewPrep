d6dcda4dba966bbf03ec6c1b547d431827d98055
Connor <connor@wybro.com>
2017-10-01 13:06:13 -0400

    Add questions & solutions

---
+++ Questions/questions12.playground/Contents.swift
@@ -0,0 +1,21 @@
+/*
+ -Given: Square 2D matrix
+     * let arr = [[1,8,10], [3,6,1], [8,2,1]]
+ -Task: Get sum for particular column
+ -Bonus points:
+     * Consider case when column is larger than arr
+*/
+
+
+
+
+/* ------------------------------------------------------
+ -Given: 2D matrix
+     * let arrSmall = [[1,2],[3,4]]
+     * let arrLarge = [[1,2,3],[4,5,6],[7,8,9]]
+ -Task: Get sum for top-left to bottom-right diagonal
+ -Bonus points:
+     * Consider case when matrix isn't square -- write func (extension?!) to check
+*/
+
+
---
+++ Solutions/questions12_solved.playground/Contents.swift
@@ -0,0 +1,62 @@
+/*
+ -Given: Square 2D matrix
+     * let arr = [[1,8,10], [3,6,1], [8,2,1]]
+ -Task: Get sum for particular column
+ -Bonus points:
+     * Consider case when column is larger than arr
+*/
+
+let arr = [[1,8,10], [3,6,1], [8,2,1]]
+
+func sum(_ column: Int, arr: [[Int]]) -> Int {
+    guard column <= arr.count else { return 0 }
+    var sum = 0
+    arr.forEach { sum += $0[column] }
+    return sum
+}
+
+//print(sum(10, arr: arr))
+//print(sum(0, arr: arr))
+//print(sum(1, arr: arr))
+//print(sum(2, arr: arr))
+
+/* ------------------------------------------------------
+ -Given: 2D matrix
+     * let arrSmall = [[1,2],[3,4]]
+     * let arrLarge = [[1,2,3],[4,5,6],[7,8,9]]
+ -Task: Get sum for top-left to bottom-right diagonal
+ -Bonus points:
+     * Consider case when matrix isn't square -- write func (extension?!) to check
+*/
+
+func sumDiagonal(arr: [[Int]]) -> Int {
+    guard arr.square() else { return 0 }
+    var sum = 0
+    arr.enumerated().forEach { index, value in
+        sum += value[index]
+    }
+    return sum
+}
+
+let arrSmall = [[1,2],[3,4]]
+let arrLarge = [[1,2,3],[4,5,6],[7,8,9]]
+//print(sumDiagonal(arr: arrSmall))
+//print(sumDiagonal(arr: arrLarge))
+
+extension Array where Element == [Int] {
+    func square() -> Bool {
+        var isSquare = true
+        forEach {
+            if $0.count != count {
+                isSquare = false
+            }
+        }
+        return isSquare
+    }
+}
+
+let notSquare = [[1,2],[1],[2,4]]
+let square = [[1,2,6], [2,3,7],[3,5,2]]
+//print(notSquare.square())
+//print(square.square())
+
